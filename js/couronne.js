dd26e3663eeef21647493b44a1584a9a263ceded
SuYeonKang <qwkrt22@gmail.com>
2021-03-18 00:11:58 +0900

    pc fin

---
+++ js/couronne.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    // activeNav();
+    scrollify();
+});
+
+
+// function activeNav(){
+//     $("header nav ul li a").click(function(){
+//         $(this).addClass("active");
+//     });
+// }
+
+function scrollify() {
+    $.scrollify({
+        section: ".panel",
+        scrollbars: false,
+        before: function (i, panels) {
+            var ref = panels[i].attr("data-section-name");
+
+            $(".pagination .active").removeClass("active");
+
+            $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+        },
+        afterRender: function () {
+            var pagination = "<ul class=\"pagination\">";
+            var activeClass = "";
+            $(".panel").each(function (i) {
+                activeClass = "";
+                if (i === 0) {
+                    activeClass = "active";
+                }
+                pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+            });
+
+            pagination += "</ul>";
+
+            $(".main").append(pagination);
+        }
+    });
+}
